545a9173563f28352dc8dbe5602b6fd1d20e1e4d
Jakob Ferdinand Wegenschimmel <jakob.wegenschimmel@rzl.at>
2025-08-26 11:19:59 +0200

    Set git pull.rebase true

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,6 +2,6 @@
 
 set -e # Exit immediately if a command exits with a non-zero status.
 
-git config --global pull.merge true
+git config --local pull.rebase true
 
 pip3 install mkdocs-material mkdocs-awesome-pages-plugin mkdocs-minify-plugin mkdocs-enumerate-headings-plugin
