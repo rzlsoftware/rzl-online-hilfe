3cc1b4ff1906e156630723e5d71e40769454b4c1
Jakob Ferdinand Wegenschimmel <jakob.wegenschimmel@rzl.at>
2025-08-28 10:59:38 +0200

    Use mcr.microsoft.com/devcontainers/python:3 as base image for devcontainer (#107)
    * Use mcr.microsoft.com/devcontainers/python:3 as base image for devcontainer
    
    * refactor: remove mkdocs feature from devcontainer and update setup script for pip installation

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,4 +4,7 @@ set -e # Exit immediately if a command exits with a non-zero status.
 
 git config --local pull.rebase true
 
-pip3 install mkdocs-material mkdocs-awesome-pages-plugin mkdocs-minify-plugin mkdocs-enumerate-headings-plugin
+pip install --upgrade pip
+
+pip install mkdocs
+pip install mkdocs-material mkdocs-awesome-pages-plugin mkdocs-minify-plugin mkdocs-enumerate-headings-plugin
